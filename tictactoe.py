14e5dec324d88f69b2c61dce77c521af2f4aecd4
ge94puh <aleks.petrov@tum.de>
2024-10-16 20:55:21 +0200

    added missing break to taking player input func, also changed the print board func to display_board, tested locally for player win, comp win and tie

--- tictactoe.py
+++ tictactoe.py
@@ -14,51 +14,64 @@ winner = None
 currentPlayer = "X"
 game_still_running = True
 
-def print_board(example_board):
+def display_board(example_board):
     for i in range(3):
         print(example_board[3*i] + "|" + example_board[3*i+1] + "|" + example_board[3*i+2])
 
 
 def take_player_input(example_board):
+    print("pick a cell: ",end="")
     while True:
         try:
             input_num = int(input())
         except:
-            print("type a number between 0 and 8")
+            print("type a number between 0 and 8 ",end="")
         else:
-            if 0 <= input_num <= 8:
+            if 0 <= input_num <= 8 and example_board[input_num] == "-":
                 example_board[input_num] = currentPlayer
+                break
             else:
-                print("the val should be between 0 and 8, try again")
+                print("the val should be between 0 and 8 and the position free, try again ",end="")
 
 
 def check_game_over(example_board):
     global winner
-    if example_board[0] == example_board[1] == example_board[2] and example_board[1] != "_":
+    if example_board[0] == example_board[1] == example_board[2] and example_board[1] != "-":
         winner = example_board[0]
+        return True
 
     elif example_board[3] == example_board[4] == example_board[5] and example_board[4] != "-":
         winner = example_board[3]
+        return True
 
     elif example_board[6] == example_board[7] == example_board[8] and example_board[7] != "-":
         winner = example_board[7]
+        return True
 
     elif example_board[0] == example_board[3] == example_board[6] and example_board[3] != "-":
         winner = example_board[3]
+        return True
 
     elif example_board[1] == example_board[4] == example_board[7] and example_board[4] != "-":
         winner = example_board[4]
+        return True
 
     elif example_board[2] == example_board[5] == example_board[8] and example_board[5] != "-":
         winner = example_board[5]
+        return True
 
     elif example_board[0] == example_board[4] == example_board[8] and example_board[4] != "-":
         winner = example_board[4]
+        return True
 
-    elif example_board[2] == example_board[4] == example_board[6] and example_board[4] != "_":
+    elif example_board[2] == example_board[4] == example_board[6] and example_board[4] != "-":
         winner = example_board[4]
+        return True
+
     elif "-" not in example_board:
         winner = None
+        return True
+
     else:
         return False
 
@@ -70,24 +83,41 @@ def switch_player():
         currentPlayer = "X"
 
 def computer(example_board):
-    while currentPlayer == "O":
+    while True:
         position = random.randint(0, 8)
         if example_board[position] == "-":
             example_board[position] = "O"
+            print(f"Computer plays {position}th position")
             switch_player()
+            break
 
+display_board(board)
 while game_still_running:
-    print_board(board)
     take_player_input(board)
-    if winner is not None:
-        if winner == "X":
-            print("X won")
+    display_board(board)
+    if check_game_over(board):
+        if winner is not None:
+            if winner == "X":
+                print("X won")
+            else:
+                print("Computer won")
         else:
-            print("Computer won")
+            print("It is a tie")
         break
+
     switch_player()
     computer(board)
+    display_board(board)
     check_game_over(board)
+    if check_game_over(board):
+        if winner is not None:
+            if winner == "X":
+                print("X won")
+            else:
+                print("Computer won")
+        else:
+            print("It is a tie")
+        break
 
 
 
